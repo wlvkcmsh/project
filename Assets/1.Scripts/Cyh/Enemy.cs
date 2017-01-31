7fea748d8e3f88dfb4c24015fa51689d79c73dae
muncleg <muncleg@naver.com>
2017-01-31 18:33:35 +0900

    no message

---
+++ Assets/1.Scripts/Cyh/Enemy.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+
+    
+    public int currentHealth ;
+
+    public void Damage(int damageAmount)
+    {
+        
+        currentHealth -= damageAmount;
+
+       
+        if (currentHealth <= 0)
+        {
+          
+            gameObject.SetActive(false);
+        }
+    }
+}
