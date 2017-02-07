a0d990441a07645c82fb3af4a28b83a72302e841
muncleg <muncleg@naver.com>
2017-02-07 13:35:58 +0900

    거리체크 한 뒤 공격

---
+++ Assets/1.Scripts/Cyh/Projectile_test.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile_test : MonoBehaviour {
+
+    float speed = 10;
+
+    public void SetSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+
+    }
+	
+	
+	void Update ()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+	}
+}
